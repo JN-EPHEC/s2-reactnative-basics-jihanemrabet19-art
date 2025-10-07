99f322d31bccb150df9001ffb716169a36e17eca
Jihane M'rabet <he135302@students.ephec.be>
2025-10-08 00:20:46 +0200

    My todo list homework

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,28 @@
-import { ScrollView, StyleSheet } from "react-native";
-
-const USERS_DATA = [
-  { id: '1', nom: 'Alice Martin', email: 'alice.martin@email.com' },
-  { id: '2', nom: 'Benjamin Dubois', email: 'ben.dubois@email.com' },
-  { id: '3', nom: 'Chloé Garcia', email: 'chloe.g@email.com' },
-  { id: '4', nom: 'David Petit', email: 'david.petit@email.com' },
-  { id: '5', nom: 'Émilie Rousseau', email: 'emilie.rousseau@email.com' },
-];
-
+import { ThemedText } from "@/components/ThemedText";
+import TodoItem from "@/components/TodoItem";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-      <ScrollView>
-        { /* Render the list of users using the UserItem component */ }
-      </ScrollView>
+    <View style={styles.mainView}>
+     <ThemedText type='title' style={styles.title}>My Todo List</ThemedText>
+     <TodoItem text="Aller dormir" />
+     <TodoItem text="Dejeuner" />
+     <TodoItem text="Aller à l'école" />
+     <TodoItem text="Apprendre react native" />
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
+
+  mainView: {
+    padding: 20
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginVertical: 20,
-  },
-  item: {
-    backgroundColor: '#ffffff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#ddd',
-  },
-  nom: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  email: {
-    fontSize: 14,
-    color: '#666',
-    marginTop: 4,
-  },
+
+  title : {
+      textAlign: "center",
+      marginBottom: 20
+  }
 });
